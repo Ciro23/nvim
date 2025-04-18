b2bf87d76b1003da62f51e25c82c5e2acc956347
ciro23 <ciro.sorre23@gmail.com>
2025-04-18 11:59:20 +0200

    change some colorscheme colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,18 @@
 return {
-  { "Mofiqul/dracula.nvim" },
+  {
+    "Mofiqul/dracula.nvim",
+    config = function()
+      local dracula = require("dracula")
+      dracula.setup({
+        colors = {
+          -- this colors are overriden because otherwise they would
+          -- be basically unreadable.
+          nontext = "#707887",
+          comment = "#919fc9",
+        },
+      })
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
