25411df6bd3068f5b264cab6c6491ff869a72957
ciro23 <ciro.sorre23@gmail.com>
2025-04-18 14:34:22 +0200

    add custom vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 12
+vim.opt.expandtab = true
+vim.opt.textwidth = 80
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.splitright = true
+vim.opt.clipboard = "unnamedplus"
